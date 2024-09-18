9377b4afd87e26d5b0b5da14f551421c36d5f397
Chinwenma <okoriechinwe1327@gmail.com>
2024-09-18 13:50:09 -0800

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "../components/Home/Nav";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <Nav />
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
